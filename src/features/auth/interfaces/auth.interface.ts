3b41d8b263676ea88e81a60dffe88b92c8c7d797
arghachdas <arghachandra@technoexponent.com>
2022-09-29 09:56:01 +0530

    routes, signup, joi validation setup

---
+++ src/features/auth/interfaces/auth.interface.ts
@@ -0,0 +1,47 @@
+import { Document } from 'mongoose';
+import { ObjectId } from 'mongodb';
+
+// adding property to Existing namaspace
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: AuthPayload; // if user is not logged in this property must not exist
+    }
+  }
+}
+
+export interface AuthPayload {
+  userId: string;
+  uId: string;
+  email: string;
+  username: string;
+  avatarColor: string;
+  iat?: number;
+}
+
+export interface IAuthDocument extends Document {
+  _id: string | ObjectId;
+  uId: string;
+  username: string;
+  email: string;
+  password?: string;
+  avatarColor: string;
+  createdAt: Date;
+  passwordResetToken?: string;
+  passwordResetExpires?: number | string;
+  comparePassword(password: string): Promise<boolean>;
+  hashPassword(password: string): Promise<string>;
+}
+
+export interface ISignUpData {
+  _id: ObjectId;
+  uId: string;
+  email: string;
+  username: string;
+  password: string;
+  avatarColor: string;
+}
+
+export interface IAuthJob {
+  value?: string | IAuthDocument;
+}
